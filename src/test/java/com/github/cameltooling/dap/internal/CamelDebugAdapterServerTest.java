45660a85f7599bc5b9c9d5e90fc0ca635d5a8521
Aurélien Pupier <apupier@redhat.com>
2022-01-07 15:44:35 +0100

    Allow to setBreakpoints
    - support a single Route and a single context for now
    - test will need to be improved to be robust
    - only Java DSL tested
    
    Signed-off-by: Aurélien Pupier <apupier@redhat.com>

--- src/test/java/com/github/cameltooling/dap/internal/CamelDebugAdapterServerTest.java
+++ src/test/java/com/github/cameltooling/dap/internal/CamelDebugAdapterServerTest.java
@@ -18,15 +18,22 @@ package com.github.cameltooling.dap.internal;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.io.File;
 import java.util.Collections;
+import java.util.Set;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 
 import org.apache.camel.CamelContext;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.impl.DefaultCamelContext;
+import org.eclipse.lsp4j.debug.Breakpoint;
 import org.eclipse.lsp4j.debug.Capabilities;
 import org.eclipse.lsp4j.debug.InitializeRequestArguments;
+import org.eclipse.lsp4j.debug.SetBreakpointsArguments;
+import org.eclipse.lsp4j.debug.SetBreakpointsResponse;
+import org.eclipse.lsp4j.debug.Source;
+import org.eclipse.lsp4j.debug.SourceBreakpoint;
 import org.eclipse.lsp4j.debug.TerminateArguments;
 import org.eclipse.lsp4j.debug.TerminatedEventArguments;
 import org.junit.jupiter.api.AfterEach;
@@ -75,7 +82,59 @@ class CamelDebugAdapterServerTest {
 			BacklogDebuggerConnectionManager connectionManager = server.getConnectionManager();
 			assertThat(connectionManager.getMbeanConnection()).as("The MBeanConnection has not been established.").isNotNull();
 			assertThat(connectionManager.getBacklogDebugger()).as("The BacklogDebugger has not been found.").isNotNull();
+			assertThat(connectionManager.getBacklogDebugger().isEnabled()).isTrue();
+			assertThat(connectionManager.getRoutesDOMDocument()).as("Routes instantiated.").isNotNull();
 		}
+	}
+	
+	@Test
+	void testSetBreakpoint() throws Exception {
+		try (CamelContext context = new DefaultCamelContext()) {
+			context.addRoutes(new RouteBuilder() {
+			
+				@Override
+				public void configure() throws Exception {
+					from("direct:testSetBreakpoint")
+						.log("Log from test"); // line number to use from here
+				}
+			});
+			int lineNumberToPutBreakpoint = 98;
+			context.start();
+			assertThat(context.isStarted()).isTrue();
+			initDebugger();
+			server.attach(Collections.singletonMap(BacklogDebuggerConnectionManager.ATTACH_PARAM_PID, Long.toString(ProcessHandle.current().pid())));
+			BacklogDebuggerConnectionManager connectionManager = server.getConnectionManager();
+			assertThat(connectionManager.getMbeanConnection()).as("The MBeanConnection has not been established.").isNotNull();
+			assertThat(connectionManager.getBacklogDebugger()).as("The BacklogDebugger has not been found.").isNotNull();
+			SetBreakpointsArguments setBreakpointsArguments = createSetBreakpointArgument(lineNumberToPutBreakpoint);
+			
+			SetBreakpointsResponse response = server.setBreakpoints(setBreakpointsArguments).get();
+			
+			Breakpoint[] responseBreakpoints = response.getBreakpoints();
+			assertThat(responseBreakpoints).hasSize(1);
+			Breakpoint responseBreakpoint = responseBreakpoints[0];
+			assertThat(responseBreakpoint.getLine()).isEqualTo(lineNumberToPutBreakpoint);
+			assertThat(responseBreakpoint.isVerified()).isTrue();
+			Set<String> breakpointsSetInCamel = connectionManager.getBacklogDebugger().breakpoints();
+			assertThat(breakpointsSetInCamel).hasSize(1);
+			
+			//TODO: trigger the route and check that we have a suspended breakpoint
+			
+		}
+	}
+
+	private SetBreakpointsArguments createSetBreakpointArgument(int lineNumberToPutBreakpoint) {
+		SetBreakpointsArguments setBreakpointsArguments = new SetBreakpointsArguments();
+		Source source = new Source();
+		String pathToItself = (new File("src/test/java/"+CamelDebugAdapterServerTest.class.getName()+".java")).getAbsolutePath();
+		source.setPath(pathToItself);
+		setBreakpointsArguments.setSource(source);
+		SourceBreakpoint[] breakpoints = new SourceBreakpoint[1];
+		SourceBreakpoint sourceBreakpoint = new SourceBreakpoint();
+		sourceBreakpoint.setLine(lineNumberToPutBreakpoint);
+		breakpoints[0] = sourceBreakpoint;
+		setBreakpointsArguments.setBreakpoints(breakpoints);
+		return setBreakpointsArguments;
 	}
 	
 	private CompletableFuture<Capabilities> initDebugger() {
